b884bba91c13a524b942d43d0638b82977099963
Kelly Young <kelly.y94@gmail.com>
2016-11-03 00:33:41 +0000

    worked on homepage form routes. Completed functionalit of login, logout, and create new user on homepage

--- server.py
+++ server.py
@@ -11,11 +11,107 @@ from model import User, Trip, Place, Category, connect_to_db, db
 
 app = Flask(__name__)
 
-app.secret_key="PX78D1EBTcu3o4v8CK6i1EvtO7N6p3Ow"
+app.secret_key = "PX78D1EBTcu3o4v8CK6i1EvtO7N6p3Ow"
 
 app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
+
+@app.route('/')
+def index():
+    """
+    Show homepage. Has login, logout, signup, create trip,
+    and go to trip. Displays depending on if username is
+    in session and if user has a trip or not.
+    """
+
+    # check to see if user is logged in
+    username = session.get('username')
+
+    # check to see if the user has a trip or not
+    user_trip = Trip.query.filter(Trip.username == username).first()
+
+    return render_template('homepage.html',
+                           username=username,
+                           user_trip=user_trip)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    """Checks login info, adds to session if successful, redirects to home."""
+
+    # get arguments from login
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # check to see if username is valid and in database
+    check_username = db.session.query(User).filter(User.username ==
+                                                   username).first()
+
+    if not username or not password:
+        flash('Please enter a username and password.')
+    elif check_username and check_username.password == password:
+        session['username'] = check_username.username
+        flash('Welcome back, %s!' % check_username.name)
+    elif check_username:
+        flash('Incorrect Password. Please try again.')
+    else:
+        flash('The username and password do not exist. Try again.')
+
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    """Remove username from session and return to homepage"""
+
+    username = session.pop('username')
+    flash('Logged out of %s.' % username)
+
+    return redirect('/')
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    """
+    If form inputs valid, creates a user and adds to the database.
+    Returns to homepage and asks user to login.
+    """
+    # get form inputs
+    name = request.form.get('name')
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    # see if user already exists
+    check_users_existance = User.query.filter(User.username == username).first()
+
+    # if username is already in the database
+    if check_users_existance:
+        flash('%s is already taken. Please try again.' % username)
+    # if username isn't taken, add to the database.
+    else:
+        new_user = User(name=name, username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('The account %s was successfully created. Please log in.' %
+              username)
+    return redirect('/')
+
+
+@app.route('/create_trip', methods=['POST'])
+def create_trip():
+    """
+    Adds trip information to database, then redirects to
+    the trip's editing page.
+    """
+    trip_name = request.form.get('tripname')
+    from_date = request.form.get('from')
+    to_date = request.form.get('to')
+
+    print trip_name, from_date, to_date
+
+    return redirect('/')
 
 if __name__ == '__main__':
 
