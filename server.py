a23e6678d998ad850f78f0ad85c615fb8770de2d
Kelly Young <kelly.y94@gmail.com>
2016-12-14 07:51:45 +0000

    playing with facebook oauth again

--- server.py
+++ server.py
@@ -3,12 +3,14 @@ import os
 
 import bcrypt
 
-# import facebook
+import facebook
 
 from jinja2 import StrictUndefined
 
 from flask import (Flask, render_template, redirect, request, flash,
                    session, jsonify, url_for, send_from_directory)
+from flask_oauth import OAuth
+
 from werkzeug import secure_filename
 
 # from flask_debugtoolbar import DebugToolbarExtension
@@ -19,6 +21,10 @@ import datetime
 
 app = Flask(__name__)
 
+oauth = OAuth()
+
+
+
 app.secret_key = "PX78D1EBTcu3o4v8CK6i1EvtO7N6p3Ow"
 
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -27,8 +33,48 @@ app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'JPG', 'PNG'])
 app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
-# --------------------------------------------------------------------------- #
+FACEBOOK_APP_ID = os.environ['FACEBOOK_APP_ID']
+FACEBOOK_APP_SECRET = os.environ['FACEBOOK_APP_SECRET']
 
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': ('age_range, gender, name, picture, id')}
+)
+# --------------------------------------------------------------------------- #
+@facebook.tokengetter
+def get_facebook_token():
+    return session.get('facebook_token')
+
+def pop_login_session():
+    session.pop('logged_in', None)
+    session.pop('facebook_token', None)
+
+@app.route("/facebook_login")
+def facebook_login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next'), _external=True))
+
+@app.route("/facebook_authorized")
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None or 'access_token' not in resp:
+        return redirect(next_url)
+
+    session['logged_in'] = True
+    session['facebook_token'] = (resp['access_token'], '')
+
+    return redirect(next_url)
+
+@app.route("/facebook_logout")
+def facebook_logout():
+    pop_login_session()
+    return redirect(url_for('index'))
 
 def allowed_file(filename):
     """check if file is a valid name
