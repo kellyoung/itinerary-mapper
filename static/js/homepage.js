f3d676e9b37c6de13cacc4fbcaf7f97381db4ad2
Kelly Young <kelly.y94@gmail.com>
2017-01-29 19:27:06 +0000

    ascii and alphanumeric verification for create account and login

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -27,14 +27,28 @@
 $('#login-form').submit(function(event){
     
 
-    var nameCheck = /([a-z])([0-9])/.test($('#login-name').val());
-    var pwCheck = /([a-z])([0-9])/.test($('#login-pw').val());
+    var nameCheck = /^[a-z0-9]+$/.test($('#login-name').val()); //alphanumeric check
+    var pwCheck = /^[\x00-\x7F]*$/.test($('#login-pw').val()); //ascii check
 
     if (nameCheck && pwCheck){
         return;
     }
     
     // give some feedback if it's not right entry
+    console.log('not valid');
+    event.preventDefault();
+
+});
+
+$('#create-form').submit(function(event){
+    var usernameCheck = /^[a-z0-9]+$/.test($('#create-uname').val()); //alphanumeric
+    var pwCheck = /^[\x00-\x7F]*$/.test($('#create-pw').val()); //ascii check
+    var nameCheck = /^[\x00-\x7F]*$/.test($('#create-name').val()); //ascii check
+
+    if (nameCheck && pwCheck && usernameCheck){
+        return;
+    }
+
     console.log('not valid');
     event.preventDefault();
 
