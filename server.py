4ac2d3971f8a956b3989e8b44fe97987924e7fb0
Kelly Young <kelly.y94@gmail.com>
2017-02-03 04:48:27 +0000

    took out all fb changes, not going to implement

--- server.py
+++ server.py
@@ -5,14 +5,14 @@ import os
 
 import bcrypt
 
-import facebook
+# import facebook
 
 from jinja2 import StrictUndefined
 
 from flask import (Flask, render_template, redirect, request, flash,
                    session, jsonify, url_for, send_from_directory)
 
-from flask_oauth import OAuth
+# from flask_oauth import OAuth
 
 from werkzeug import secure_filename
 
@@ -33,58 +33,7 @@ app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'JPG', 'PNG'])
 app.jinja_env.undefined = StrictUndefined
 app.jinja_env.auto_reload = True
 
-oauth = OAuth()
 
-FACEBOOK_APP_ID = os.environ['FACEBOOK_APP_ID']
-FACEBOOK_APP_SECRET = os.environ['FACEBOOK_APP_SECRET']
-
-facebook = oauth.remote_app('facebook',
-    base_url='https://graph.facebook.com/',
-    request_token_url=None,
-    access_token_url='/oauth/access_token',
-    authorize_url='https://www.facebook.com/dialog/oauth',
-    consumer_key=FACEBOOK_APP_ID,
-    consumer_secret=FACEBOOK_APP_SECRET,
-    request_token_params={'scope': ('email, ')}
-)
-
-
-# --------------------------------------------------------------------------- #
-
-
-@facebook.tokengetter
-def get_facebook_token():
-    return session.get('facebook_token')
-
-
-def pop_login_session():
-    session.pop('logged_in', None)
-    session.pop('facebook_token', None)
-
-
-@app.route("/facebook_login")
-def facebook_login():
-    return facebook.authorize(callback=url_for('facebook_authorized',
-                              next=request.args.get('next'), _external=True))
-
-
-@app.route("/facebook_authorized")
-@facebook.authorized_handler
-def facebook_authorized(resp):
-    next_url = request.args.get('next') or url_for('index')
-    if resp is None or 'access_token' not in resp:
-        return redirect(next_url)
-
-    session['logged_in'] = True
-    session['facebook_token'] = (resp['access_token'], '')
-
-    return redirect(next_url)
-
-
-@app.route("/fb_logout")
-def fb_logout():
-    pop_login_session()
-    return redirect(url_for('index'))
 
 
 def allowed_file(filename):
@@ -153,7 +102,6 @@ def login():
 @app.route('/logout')
 def logout():
     """Remove username from session and return to homepage"""
-
     session.pop('username')
     flash('Logged out.')
 
--- static/js/fb_login.js
+++ static/js/fb_login.js
@@ -1,6 +1,4 @@
-
-
-// This is called with the results from from FB.getLoginStatus().
+  // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
@@ -8,33 +6,21 @@
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
-    $(document).ajaxSuccess(function() {
-        window.location.reload(true);
-    });
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      var params = {
-          "user_id": response.authResponse.userID,
-          "access_token": response.authResponse.accessToken
-        };
-      $.post('/fb_login.json', params, function(result){
-          console.log(result.status);
-      });
-
       testAPI(response);
+      // window.location = '/fblogin';
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('fb-status').innerHTML = 'Please log ' +
+      document.getElementById('status').innerHTML = 'Please log ' +
         'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('fb-status').innerHTML = 'Please log ' +
+      document.getElementById('status').innerHTML = 'Please log ' +
         'into Facebook.';
     }
   }
-
-
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
@@ -43,50 +29,65 @@
       statusChangeCallback(response);
     });
   }
-
-
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '1200587173334573',
     cookie     : true,  // enable cookies to allow the server to access 
                         // the session
     xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use graph api version 2.8
+    version    : 'v2.5' // use graph api version 2.5
   });
-
-
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
   FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
     
   });
   };
-
   // Load the SDK asynchronously
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
+    fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
+  
+
 
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
-
- 
   function testAPI(response) {
+    
+    var token = response['authResponse']['accessToken'];
+    var userID = response['authResponse']['userID'];
+    console.log(response);
 
-    // var token = response.authResponse.accessToken;
-    // console.log(token);
-    // var params = { 'token': response.authResponse.accessToken};
-    // console.log(params);
-    // $.post('/fb_login.json', params, function(){
-    //     console.log('test');
-    // });
 
-    FB.api('/me', {fields: 'name, id'}, function(response) {
+
+    var result = {'fb_token': token, 'user_id': userID};
+
+
+    FB.api('/me', function(response) { 
       console.log('Successful login for: ' + response.name);
-      document.getElementById('fb-status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+      
+      result['name'] = response.name;
+      console.log(result);
+
+      $.post("/fblogin", result, function(){
+        console.log('post request');
+        window.load = '/fblogin';
+
+      });
     });
+
   }
